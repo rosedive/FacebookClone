2592ac98534c9fd599eae42cb0d8031240321cc4
kavunap <pkavuna@gmail.com>
2019-09-04 08:32:58 +0000

    error pages implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,13 +63,11 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :content, :image, :user_id)
-    end
+  def post_params
+    params.require(:post).permit(:title, :content, :image, :user_id)
+  end
 end
